260cb4eaa46bc49cf1c5fb267fef1ca82d324f01
jinii0412 <jiniii0412@gmail.com>
2020-10-04 20:54:57 +0900

    Commit

---
+++ src/main/java/com/jin/kabang/springbootstudysample/SpringbootstudysampleApplication.java
@@ -0,0 +1,13 @@
+package com.jin.kabang.springbootstudysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstudysampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstudysampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jin/kabang/springbootstudysample/SpringbootstudysampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.kabang.springbootstudysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstudysampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
